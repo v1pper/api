92b22ae3d21e15867a542ade12be96ebaac71fd7
v1pper <vova22089@gmail.com>
2025-05-06 17:38:41 +0300

    new

--- anime/anime/urls.py
+++ anime/anime/urls.py
@@ -1,18 +1,7 @@
 from django.urls import path, include
 from django.contrib import admin
-<<<<<<< HEAD
 
 urlpatterns = [
     path('api/', include("api.urls")), 
     path('admin/', admin.site.urls),
-=======
-from api.views import *
-
-urlpatterns = [
-    path('admin/',admin.site.urls),
-    path('anime/achievements/', anime_achievements),
-    path('anime/achievements/completed/', completed_achievements),
-    path('anime/<str:tag_name>/<int:anime_id>/achievements/', anime_tag_achievements),
-    path('api/achievements/',create_achievement),
->>>>>>> ca88e7910aa161598d49bac1b65aacb816dc4efd
 ]
